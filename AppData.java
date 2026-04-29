65c269dc830624c95b71eb6072f1c6ef3e1dcf34
NiilaHolster <holster.niila@gmail.com>
2026-04-29 09:54:56 +0300

    Update AppData.java
    Replaced taskNames with taskKeys for predefined tasks.

--- AppData.java
+++ AppData.java
@@ -5,27 +5,33 @@ import java.util.List;
 import java.util.Map;
 
 public class AppData {
-    public static List<String> taskNames = new ArrayList<>();
+
+   
+    public static List<String> taskKeys = new ArrayList<>();
     public static List<String> customTasks = new ArrayList<>();
+
     public static Map<LocalDate, Integer> dailyTaskCount = new HashMap<>();
     public static Map<LocalDate, List<String>> dailyCompletedTasks = new HashMap<>();
 
     public static void initializePredefinedTasks() {
-        taskNames.add("Task 1: Clean Kitchen");
-        taskNames.add("Task 2: Vacuum The House");
-        taskNames.add("Task 3: Cooking");
-        taskNames.add("Task 4: Take the dog out");
-        taskNames.add("Task 5: Do Programming 4 homework");
-        taskNames.add("Task 6: Take trash Out");
-        taskNames.add("Task 7: Go to Gym");
-        taskNames.add("Task 8: Do Project work");
-        taskNames.add("Task 9: Clean Dust");
-        taskNames.add("Task 10: Clean the bathroom");
-        taskNames.add("Task 11: Dishes");
-        taskNames.add("Task 12: Do laundry");
-        taskNames.add("Task 13: Go walking");
-        taskNames.add("Task 14: Make dinner");
-        taskNames.add("Task 15: Call relatives");
-        taskNames.add("Task 15: Brush Teeth");
+
+        taskKeys.clear();
+
+        taskKeys.add("task.clean_kitchen");
+        taskKeys.add("task.vacuum_house");
+        taskKeys.add("task.cooking");
+        taskKeys.add("task.walk_dog");
+        taskKeys.add("task.homework");
+        taskKeys.add("task.trash_out");
+        taskKeys.add("task.gym");
+        taskKeys.add("task.project_work");
+        taskKeys.add("task.clean_dust");
+        taskKeys.add("task.clean_bathroom");
+        taskKeys.add("task.dishes");
+        taskKeys.add("task.laundry");
+        taskKeys.add("task.walking");
+        taskKeys.add("task.make_dinner");
+        taskKeys.add("task.call_relatives");
+        taskKeys.add("task.brush_teeth");
     }
 }
